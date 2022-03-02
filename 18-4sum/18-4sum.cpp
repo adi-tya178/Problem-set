758cf72a3c7689f800912e59605acd7b78282743
adi-tya178 <67846260+adi-tya178@users.noreply.github.com>
2022-03-02 22:37:23 +0530

    Time: 320 ms (21.24%), Space: 49.5 MB (10.35%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,28 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
+        set<vector<int>> s;
+        sort(nums.begin(),nums.end());
         int n = nums.size();
-        vector<vector<int>> ans;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                int l = j + 1, r = n - 1;
-                int remain = target - nums[i] - nums[j];
-                while (l < r) {
-                    if (nums[l] + nums[r] == remain) {
-                        ans.push_back({nums[i], nums[j], nums[l], nums[r]});
-                        ++l; --r;
-                        while (l < r && nums[l-1] == nums[l]) ++l; // Skip duplicate nums[l]
-                    } else if (nums[l] + nums[r] > remain) {
-                        --r;
-                    } else {
-                        ++l;
+        for(int a=0;a<n;a++)
+        {
+            for(int b=a+1;b<n-1;b++)
+            {
+                int c = b+1;
+                int d = n-1;
+                while(c<d)
+                {
+                   long long sum = (long long )nums[a] + nums[b] + nums[c] + nums[d];
+                    
+                    if(sum==target)
+                    {
+                        vector<int> temp = {nums[a],nums[b],nums[c],nums[d]};
+                        s.insert(temp);
+                        d--;
+                        c++;
+                    }
+                    else if(sum>target)
+                    {
+                        d--;
+                    }
+                    else
+                    {
+                        c++;
                     }
                 }
-                while (j+1 < n && nums[j] == nums[j+1]) ++j; // Skip duplicate nums[j]
             }
-            while (i+1 < n && nums[i] == nums[i+1]) ++i; // Skip duplicate nums[i]
         }
+       vector<vector<int>> ans;
+        for(auto x:s)
+        {
+            ans.push_back(x);
+        }
+        
         return ans;
+        
     }
 };
