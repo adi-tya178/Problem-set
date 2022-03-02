27b57afbd500fbee1ff80b5358a3dbd88b1b38ab
adi-tya178 <67846260+adi-tya178@users.noreply.github.com>
2022-03-02 17:18:58 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (72.95%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0;
+     
+        for(auto x:t)
+        {
+            if(x==s[i])
+            {
+                i++;
+            }
+        }
+        return i==s.length();
+    }
+};
