ce9d95bf6b25f1a1531858ef04ea145262f7fc62
adi-tya178 <67846260+adi-tya178@users.noreply.github.com>
2022-03-05 12:44:44 +0530

    Time: 15 ms (39.06%), Space: 9.5 MB (74.40%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        int maxval = INT_MIN;
+        for(auto x: nums)
+            maxval = maxval < x ? x : maxval; 
+        int n = maxval+1;
+        vector<int> sum(n,0);
+        vector<int> dp(n,0);
+        
+        for(auto x:nums)
+        {
+            sum[x] +=x;
+        }
+        dp[0] = 0;
+        dp[1] = sum[1];
+        for(int i=2;i<n;i++)
+        {
+            dp[i] = max(dp[i-2]+sum[i],dp[i-1]);
+        }
+        return dp[n-1];
+    }
+};
