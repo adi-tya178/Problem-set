b43d55a6fadb6d2a6d2042080937235fc7e25f96
adi-tya178 <67846260+adi-tya178@users.noreply.github.com>
2022-03-31 00:20:02 +0530

    Time: 76 ms (82.28%), Space: 32.4 MB (36.31%) - LeetHub

---
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int maxArea(int h, int w, vector<int>& hc, vector<int>& vc) {
+        long long mod = 1e9 + 7;
+        sort(hc.begin(),hc.end());
+        sort(vc.begin(),vc.end());
+        int maxih=INT_MIN,maxiv = INT_MIN;
+        for(int i=0;i<=hc.size();i++)
+        {
+            int diff=0;
+            if(i==0) diff = hc[i];
+            else if(i==hc.size()) diff = h - hc[i-1];
+            else
+            {
+                diff = hc[i] - hc[i-1];
+            }
+           // cout<<diff<<" ";
+            maxih = max(maxih,diff);
+        }
+        //cout<<endl;
+         for(int i=0;i<=vc.size();i++)
+        {
+            int diff=0;
+            if(i==0)diff = vc[i];
+            else if(i==vc.size()) diff = w - vc[i-1];
+            else
+            {
+                diff = vc[i] - vc[i-1];
+            }
+            
+            maxiv = max(maxiv,diff);
+        }
+        
+//         maxiv = maxiv%mod;
+//         maxih = maxih%mod;
+        
+//         int ans =  (maxiv*maxih)%mod;
+        return ((maxih%mod) * (maxiv%mod)) % mod;
+        // return ans;
+    }
+};
