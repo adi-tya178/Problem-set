44f89c12f727687c36415f54150ce02417cc93e0
adi-tya178 <67846260+adi-tya178@users.noreply.github.com>
2022-06-17 22:01:36 +0530

    Time: 20 ms (8.53%), Space: 16.9 MB (17.04%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    void helper(vector<int> nums,vector<int> &temp,set<vector<int>> &ans,int i,int n)
-    {
-        if(i==n)
-        {
-            ans.insert(temp);
-        
-            return ;
-        }
-        temp.push_back(nums[i]);
-        helper(nums,temp,ans,i+1,n);
-        temp.pop_back();
-        helper(nums,temp,ans,i+1,n);
+   void helper(vector<int> num,int ind,set<vector<int>> &ans,vector<int> &temp)
+{
+    int n = num.size();
+    if(ind == n){
+        ans.insert(temp);
+        return;
     }
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<int> temp;
-       set<vector<int>> ans;
-        int n = nums.size();
-        helper(nums,temp,ans,0,n);
-        vector<vector<int>> res;
-        for(auto x:ans)
-        {
-            res.push_back(x);
-        }
-        return res;
+    temp.push_back(num[ind]);
+    helper(num,ind+1,ans,temp);
+    temp.pop_back();
+    helper(num,ind+1,ans,temp);
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+       set<vector<int>> v;
+    vector<int> temp;
+    helper(arr,0,v,temp);
+    vector<vector<int>> ans;
+    for(auto x:v)
+    {
+        ans.push_back(x);
+    }
+   // sort(ans.begin(),ans.end());
+  //  ans.erase(ans.begin());
+    return ans;
     }
 };
