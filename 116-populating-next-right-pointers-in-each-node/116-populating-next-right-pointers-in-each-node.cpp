2624573034e49fec6ec13783bbda0e225329fa7d
adi-tya178 <67846260+adi-tya178@users.noreply.github.com>
2022-06-27 10:34:42 +0530

    Time: 31 ms (43.02%), Space: 16.7 MB (96.63%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -20,16 +20,19 @@ class Solution {
 public:
     Node* connect(Node* root) {
         if(root==NULL)return NULL;
-         if(root->left)
-         {
-             root->left->next = root->right;
-         
-        if(root->next)
-        {
-            root->right->next = root->next->left;}
         
+        if(root->left)
+        {
+          root->left->next = root->right;
+        
+          if(root->next)
+        {
+            root->right->next = root->next->left;
+        }
+       
         connect(root->left);
-        connect(root->right);}
-        return root;    
+        connect(root->right);
+        }
+        return root;
     }
 };
