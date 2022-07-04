d3d3ee5e17e045a48349279261ae81c967e33c2f
Aditya Harsh <67846260+adi-tya178@users.noreply.github.com>
2022-07-05 00:35:09 +0530

    Added solution - LeetHub

--- Job Sequencing Problem - GFG/job-sequencing-problem.cpp
+++ Job Sequencing Problem - GFG/job-sequencing-problem.cpp
@@ -26,31 +26,44 @@ struct Job
 class Solution 
 {
     public:
-    //Function to find the maximum profit and the number of jobs done.
-    static bool cmp(Job a,Job b){
+    static bool cmp(Job a, Job b)
+    {
+        if(a.profit==b.profit)return a.dead>b.dead;
         return a.profit>b.profit;
     }
+    //Function to find the maximum profit and the number of jobs done.
     vector<int> JobScheduling(Job arr[], int n) 
     { 
         // your code here
         sort(arr,arr+n,cmp);
-        bool done[n] = {0};
-        int day=0,profit = 0;
+        int maxDays = INT_MIN;
         for(int i=0;i<n;i++)
         {
-            for(int j=min(n,arr[i].dead-1);j>=0;j--)
+            maxDays = max(maxDays,arr[i].dead);
+        }
+        
+      vector<int> time(maxDays+1,-1);
+        int total=0;
+        int maxProfit = 0;
+        
+        for(int i=0;i<n;i++)
+        {
+            Job temp = arr[i];
+            
+            for(int k = temp.dead;k>0;k--)
             {
-                if(done[j]==false)
+                if(time[k]==-1)
                 {
-                    day+=1;
-                    profit+=arr[i].profit;
-                    done[j] = true;
+                    time[k] = i;
+                    total++;
+                    maxProfit+=temp.profit;
                     break;
                 }
             }
         }
-        return {day,profit};
         
+        
+        return {total,maxProfit};
         
     } 
 };
